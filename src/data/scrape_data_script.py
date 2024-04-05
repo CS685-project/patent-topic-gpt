a23b4e297d100efaa08fadbb5e81219f8ccc9df2
MihirT906 <mihir.thalanki@gmail.com>
2024-04-05 19:52:45 -0400

    Update script and add data from 2020

--- src/data/scrape_data_script.py
+++ src/data/scrape_data_script.py
@@ -1,21 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-import requests
+
+# Load existing links from file
+existing_links = set()
+try:
+    with open('links.txt', 'r') as file:
+        for line in file:
+            existing_links.add(line.strip())
+except FileNotFoundError:
+    pass
 
 url = 'https://developer.uspto.gov/product/patent-grant-full-text-dataxml#product-files'
 driver = webdriver.Chrome()
 driver.get(url)
-time.sleep(5)
+time.sleep(20)
 
 rows = driver.find_elements(by='xpath', value='//*[@id="table"]/tbody/tr/td/a')
-print(len(rows))
-links = []
-for row in rows:
-    links.append(row.get_attribute('href'))
+print("Number of links extracted:", len(rows))
 
-print(links)
-    
-# page_link = driver.find_elements(by='xpath', value='/html/body/div[1]/div[3]/div/section/div[2]/article/div[2]/div[3]/div[1]/form[2]/div[1]/div[3]/div[1]/span[2]/span/button')
-# page_link[0].click()
-# time.sleep(5)
+
+# Extract new links
+new_links = set()
+for row in rows:
+    new_links.add(row.get_attribute('href'))
+
+# Combine existing links with new links
+links = existing_links.union(new_links)
+
+# Write all links to file
+with open('links.txt', 'w') as file:
+    for link in links:
+        file.write(link + '\n')
+
+print("Links saved to links.txt")
+
+driver.quit()
